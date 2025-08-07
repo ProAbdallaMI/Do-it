36e29f9566be1adf0ebf1f3ffeeabd5bef68fdac
ProAbdallaMI <proabdallami@gmail.com>
2025-08-07 10:32:41 +0300

    footer and navbar
    - add footer with my unique identity
    - setup navbar

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,9 @@
 export default function Footer() {
-    return ( <footer className="">Footer Content</footer> )
+	return (
+		<footer className="fixed bottom-0 left-0 right-0 text-[8px] text-center">
+			<div>
+				Copyright © 2025 saved for <span>ABDALLA MOHAMED ASHAG</span>
+			</div>
+		</footer>
+	);
 }
