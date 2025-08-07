3f34d87a2acb34fc7eefa6db3551286b01274a88
ProAbdallaMI <proabdallami@gmail.com>
2025-08-07 07:21:49 +0300

    colors & main structure
    - customizing colors we will work with
    - build the main app structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
+import Footer from "./components/Footer";
+import List from "./components/List";
+import Navbar from "./components/Navbar";
 
 function App() {
-  
-
-  return (
-    <>
-      <h1 className='text-3xl font-bold'>Hello World</h1>
-    </>
-  )
+	return (
+		<>
+			<Navbar />
+			<List />
+			<Footer />
+		</>
+	);
 }
 
-export default App
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import LuMenu from "react-icons/lu";
+
+export default function Navbar() {
+	return (
+		<nav className="bg-gray-800 p-4">
+			<div className="">
+				<LuMenu />
+				<h1>Do it</h1>
+			</div>
+		</nav>
+	);
+}
