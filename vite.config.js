13c632d199b6577b62f4bf0e90347223ae128671
ProAbdallaMI <proabdallami@gmail.com>
2025-08-11 00:41:19 +0300

    - add https connection

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import mkcert from 'vite-plugin-mkcert'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/Do-it/',
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), mkcert()],
   server: {
     port: 5173,
     host: '0.0.0.0',
