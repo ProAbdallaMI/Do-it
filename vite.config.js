354ffee602baeb4a8eda6d2c74ec10bbd41b0563
ProAbdallaMI <proabdallami@gmail.com>
2025-08-12 14:16:59 +0300

    remove https connection

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import mkcert from 'vite-plugin-mkcert'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/Do-it/',
-  plugins: [react(), tailwindcss(), mkcert()],
+  plugins: [react(), tailwindcss()],
   server: {
     port: 5173,
     host: '0.0.0.0',
