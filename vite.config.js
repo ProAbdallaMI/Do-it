9a30e185a11c227108397700051e240dd9639cfd
ProAbdallaMI <proabdallami@gmail.com>
2025-08-13 07:50:45 +0300

    adding Progressive web App capapility

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,39 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
+import mkcert from "vite-plugin-mkcert";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Do-it/',
-  plugins: [react(), tailwindcss()],
-  server: {
-    port: 5173,
-    host: '0.0.0.0',
-  },
-})
+	base: "/Do-it/",
+	plugins: [
+		react(),
+		tailwindcss(),
+		mkcert(),
+		VitePWA({
+			registerType: "autoUpdate",
+			manifest: {
+				name: "Do it",
+				short_name: "Do-it",
+				description: "task management app",
+				theme_color: "#ffffff",
+				background_color: "#ffffff",
+				display: "standalone",
+				start_url: "/Do-it/",
+				scope: "/Do-it/",
+				icons: [
+					{
+						src: "/Do-it/pwa-192x192.png",
+						sizes: "200x200",
+						type: "image/png",
+					},
+				],
+			},
+		}),
+	],
+	server: {
+		port: 5173,
+		host: "0.0.0.0",
+	},
+});
